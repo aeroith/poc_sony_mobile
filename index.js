0d5c0307416e998a8b3c4de2728ffce8c4d7e064
truluyub <bugra.uluyurt@sony.com>
2018-02-12 21:18:07 +0300

    Expo configurations finished

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import { AppRegistry } from 'react-native';
+// Put export import here to see device logs at Expo XDE
+// import Expo from 'expo';
 import App from './src';
 
 AppRegistry.registerComponent('RNBoilerplate', () => App);
