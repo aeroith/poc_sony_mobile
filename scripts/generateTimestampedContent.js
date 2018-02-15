78fa9f5845a787898444239dcb3b380c64c337ad
Serhat Cillidag <serhat.cillidag@sony.com>
2018-02-15 08:54:28 +0300

    [SMC-13] Add hot reloading to db.json

--- scripts/generateTimestampedContent.js
+++ scripts/generateTimestampedContent.js
@@ -1,14 +1,15 @@
 const fs = require('fs');
 const { exec } = require('child_process');
-const db = require('../db');
+const chokidar = require('chokidar');
 
+const log = console.log.bind(console);
 const modifiedDbPath = `${__dirname}/modifieddb.json`;
 let port = 3005;
 
 const toModify = ['nextAiring'];
 
+// parse --port argument
 const args = process.argv;
-
 if (args.length > 2) {
   const portArg = args.find(a => a.indexOf('--port') > -1);
   if (!portArg) return console.error('Wrong arguments');
@@ -19,22 +20,31 @@ if (args.length > 2) {
 }
 
 const currentTime = Math.round(new Date().getTime() / 1000.0);
-const content = db.content
-  .map(c => toModify
-    .reduce((acc, val) => Object.assign({}, acc, { [val]: (currentTime + c[val] + (3600 * c.id)) }), c));
+
+
+const getDb = () => JSON.parse(fs.readFileSync(`${__dirname}/../db.json`));
+const getDbContent = () => {
+  const db = getDb();
+  const content = db.content
+    .map(c => toModify
+      .reduce((acc, val) => Object.assign({}, acc, { [val]: (currentTime + c[val] + (3600 * c.id)) }), c));
+  return Object.assign({}, db, { content })
+};
+
 
 const deleteFile = path => fs.unlinkSync(path);
 
 const runScript = (script) => {
-  const jsonServer = exec(script, (err, stdout) => {
+  this.jsonServer = exec(script, (err, stdout) => {
     if (err) return;
     console.log(stdout);
   });
-  jsonServer.on('exit', () => console.log('\nJson-server has stopped.'));
+  this.jsonServer.on('exit', () => console.log('\nJson-server has stopped.'));
 
   // capture the ctrl-c on main process
   process.on('SIGINT', () => {
-    jsonServer.kill();
+    this.jsonServer.kill();
+    this.watcher.close();
   });
 
   // handle exiting gracefully and remove modifieddb file
@@ -43,8 +53,16 @@ const runScript = (script) => {
   });
 };
 
-return fs.writeFile(modifiedDbPath, JSON.stringify(Object.assign({}, db, { content })), 'utf-8', (err) => {
+return fs.writeFile(modifiedDbPath, JSON.stringify(getDbContent()), 'utf-8', (err) => {
   if (err) return console.log(err);
+  this.watcher = chokidar.watch(`${__dirname}/../db.json`);
+  this.watcher
+    .on('change', () => {
+      fs.writeFileSync(modifiedDbPath, JSON.stringify(getDbContent()), 'utf-8');
+    })
+    .on('error', (error) => {
+      log(`Watcher error: ${error}`);
+    });
   console.log(`Running json-server at port: ${port}. \nVisit http://localhost:${port}/content to see it!`);
   runScript(`json-server --port=${port} --watch ${modifiedDbPath}`);
 });
