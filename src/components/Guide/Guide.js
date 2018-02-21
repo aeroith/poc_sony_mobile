d61f9a1a06ec78e16896c907f917db83f1102573
Serhat Cillidag <serhat.cillidag@sony.com>
2018-02-21 11:23:26 +0300

    [SMC-4] Fix background color when no content is present

--- src/components/Guide/Guide.js
+++ src/components/Guide/Guide.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, ScrollView, Text, Image, TouchableOpacity } from 'react-native';
+import { View, ScrollView } from 'react-native';
 import styles from './styles';
 import GuideItem from '../GuideItem';
 
--- src/containers/Guide/styles.js
+++ src/containers/Guide/styles.js
@@ -1,9 +1,12 @@
 import { StyleSheet } from 'react-native';
+import colorPalette from '../../config/colorPalette';
+
 
 const styles = StyleSheet.create({
   mainContainer: {
     flex: 1,
     width: '100%',
+    backgroundColor: colorPalette.som
   }
 });
 
