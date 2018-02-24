88d1402ebdb939030540e283e442b71291764af0
Serhat Cillidag <serhat.cillidag@sony.com>
2018-02-24 18:14:31 +0300

    [SMC-20] Add notification packages

--- android/app/src/main/java/com/rnboilerplate/MainApplication.java
+++ android/app/src/main/java/com/rnboilerplate/MainApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import com.oblador.vectoricons.VectorIconsPackage;
+import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
+import com.geektime.rnonesignalandroid.ReactNativeOneSignalPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -25,6 +27,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativePushNotificationPackage(),
+            new ReactNativeOneSignalPackage(),
             new VectorIconsPackage(),
             new LinearGradientPackage()
       );
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { BackHandler, StatusBar, View } from 'react-native';
 import { NavigationActions, addNavigationHelpers } from 'react-navigation/src/react-navigation';
+import OneSignal from 'react-native-onesignal'; // Import package from node modules
 import { connect } from 'react-redux';
 import AppNavigator from '../navigator';
 import { addListener } from '../index';
@@ -20,6 +21,13 @@ export default class App extends Component {
     nav: PropTypes.object.isRequired,
   };
 
+  componentWillMount() {
+    OneSignal.addEventListener('received', this.onReceived);
+    OneSignal.addEventListener('opened', this.onOpened);
+    OneSignal.addEventListener('registered', this.onRegistered);
+    OneSignal.addEventListener('ids', this.onIds);
+  }
+
   componentDidMount() {
     this.backHandler = BackHandler.addEventListener('hardwareBackPress', () => {
       this.props.dispatch(NavigationActions.back());
@@ -29,7 +37,30 @@ export default class App extends Component {
 
   componentWillUnmount() {
     this.backHandler.remove();
+    OneSignal.removeEventListener('received', this.onReceived);
+    OneSignal.removeEventListener('opened', this.onOpened);
+    OneSignal.removeEventListener('registered', this.onRegistered);
+    OneSignal.removeEventListener('ids', this.onIds);
   }
+
+  onReceived = (notification) => {
+    console.log('Notification received: ', notification);
+  };
+
+  onOpened = (openResult) => {
+    console.log('Message: ', openResult.notification.payload.body);
+    console.log('Data: ', openResult.notification.payload.additionalData);
+    console.log('isActive: ', openResult.notification.isAppInFocus);
+    console.log('openResult: ', openResult);
+  };
+
+  onRegistered = (notifData) => {
+    console.log('Device had been registered for push notifications!', notifData);
+  };
+
+  onIds = (device) => {
+    console.log('Device info: ', device);
+  };
 
   render() {
     const { dispatch, nav } = this.props;
