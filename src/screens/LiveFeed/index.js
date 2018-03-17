960b9487e1de791ba4944c69b7d1cc852ce01f40
Serhat Cillidag <serhat.cillidag@sony.com>
2018-03-17 19:58:40 +0300

    [SMC-29] Add missing props

--- src/screens/LiveFeed/index.js
+++ src/screens/LiveFeed/index.js
@@ -38,6 +38,7 @@ export default class LiveFeed extends Component {
           paused
           translate={this.props.translate}
           source={{ uri: this.props.liveUrl }}
+          {...this.props}
         />
       </View>
     );
